9098ca2604813cb91e64c349f353ed961f891a3b
Naveen Kumar BV <naveenkumarbv66@gmail.com>
2025-09-02 17:02:38 +0530

    box layout demo

--- app/src/main/java/com/naveen/androidjetpacklayouts/screens/BoxLayoutScreen.kt
+++ app/src/main/java/com/naveen/androidjetpacklayouts/screens/BoxLayoutScreen.kt
@@ -1,12 +1,18 @@
 package com.naveen.androidjetpacklayouts.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -29,4 +35,46 @@ fun BoxLayoutScreen() {
             modifier = Modifier.align(Alignment.Center)
         )
     }
+}
+
+
+@Composable
+fun BoxLayoutTwoScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(Color.Gray)
+    ) {
+        Surface(
+            modifier = Modifier
+                .height(50.dp)
+                .width(100.dp)
+                .align(Alignment.Center),
+            color = Color.Red
+        ) { }
+
+        Surface(
+            modifier = Modifier
+                .height(50.dp)
+                .width(100.dp)
+                .align(Alignment.TopEnd),
+            color = Color.Cyan
+        ) { }
+
+        Surface(
+            modifier = Modifier
+                .height(50.dp)
+                .width(100.dp)
+                .align(Alignment.BottomStart),
+            color = Color.Blue
+        ) { }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun BoxLayoutScreenDemo(){
+    BoxLayoutTwoScreen()
 }
